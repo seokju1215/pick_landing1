3f43c963f5901989d43884d615e8c633c9d164ab
홍석주 <hsj202201@gmail.com>
2025-03-21 12:53:53 +0900

    LandingMobile 컴포넌트

--- src/components/mobile/LandingMobile.jsx
+++ src/components/mobile/LandingMobile.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import mobileview_booklanding from '../../assets/mobileview_booklanding.svg'
 
 function LandingMobile(){
     return(
-        <div>모바일 ㅇㅇ</div>
+        <div style = {{display : 'flex', justifyContent : 'center', padding : '72px 0px', width : "100%", alignItems: 'center'}} >
+        <img src = {mobileview_booklanding} alt = "mobileview_booklanding" style={{width : "90%", maxWidth : "400px"}}/>
+        </div>
     );
 }
 
