02b28273b07350d5a15e886a3243b277f628f11c
홍석주 <hsj202201@gmail.com>
2025-03-21 16:21:55 +0900

    내용 수정

--- src/components/desktop/LandingDesktop.jsx
+++ src/components/desktop/LandingDesktop.jsx
@@ -10,12 +10,12 @@ function LandingDesktop() {
     const fingerprint = useFingerprint();
 
     return (
-        <div style={{ display: "flex", flexDirection: "row", justifyContent: 'center', alignItems: 'center', height: '100vh', width: "100%", gap: '250px' }}>
+        <div style={{ display: "flex", flexDirection: "row", justifyContent: 'center', alignItems: 'center', height: '100vh', width: "100%", gap: '150px' }}>
             <div style={{ display : "flex", flexDirection : "column"}}>
                 <img src={DesktopLeftView} alt="DesktopLeftView" style={{width: "500px", marginBottom : "30px"}} />
                 <ApplyButton width = "400px" height = "60px" fontSize = "20px" onClick={() => setIsModalOpen(true)}/>
             </div>
-            <img src={DesktopRightView} alt="DesktopRightimage" style={{width: "300px" }} />
+            <img src={DesktopRightView} alt="DesktopRightimage" style={{width: "600px" }} />
             <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} fingerprint = {fingerprint} />
         </div>
     );
