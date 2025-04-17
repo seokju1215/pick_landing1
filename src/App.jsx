e0217ad4ff4fc2f315d60157c6de11db7cfce661
홍석주 <hsj202201@gmail.com>
2025-04-18 00:27:11 +0900

    기본 라우트를 /folk-landing으로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import useIsMobile from './hooks/useIsMobile'
-import LandingDesktop from './components/desktop/LandingDesktop'
-import LandingMobile from './components/mobile/LandingMobile'
+import { BrowserRouter as Router, Route, Navigate, Routes } from 'react-router-dom'; // Redirect 대신 Navigate 사용
+import useIsMobile from './hooks/useIsMobile';
+import LandingDesktop from './components/desktop/LandingDesktop';
+import LandingMobile from './components/mobile/LandingMobile';
 
 function App() {
   const isMobile = useIsMobile();
 
   return (
-    <>
-      {isMobile ? <LandingMobile /> : <LandingDesktop />}
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/folk-landing" />} />
+        <Route path="/folk-landing" element={isMobile ? <LandingMobile /> : <LandingDesktop />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
