21b88d24d8505e655a7ebec8e426f04ee62b1490
홍석주 <hsj202201@gmail.com>
2025-04-18 12:06:26 +0900

    배포 환경에서 api key 잘 들어가나 확인

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -10,6 +10,8 @@ const firebaseConfig = {
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+console.log("🔥 apiKey:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("🔥 projectId:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
 
 // Firebase 앱 초기화
 const app = initializeApp(firebaseConfig);
