2d0f4a4fb10664b98f37d2fa045f58f5d6f95cfc
홍석주 <hsj202201@gmail.com>
2025-04-18 11:33:41 +0900

    아마 완성

--- src/components/mobile/LandingMobile.jsx
+++ src/components/mobile/LandingMobile.jsx
@@ -28,7 +28,7 @@ function LandingMobile() {
                     alt="mobileview"
                     style={{ width: '90%', maxWidth: '400px' }}
                 />
-                <div style={{ display: 'flex', flexDirection: "row", gap: "15px", marginTop: "50px", width: '90%', maxWidth: '400px' }}>
+                <div style={{ display: 'flex', flexDirection: "row", gap: "10px", marginTop: "50px", width: '100%', maxWidth: '400px' }}>
                     <div style={{ flex: 1 }}>
                         <img src={DesktopLeft1} alt="mobileleft" style={{ width: '100%', height: 'auto', border : '1px solid #E8E8E8'}} />
                     </div>
