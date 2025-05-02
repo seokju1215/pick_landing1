18598e4825707bc9097e943ff188b85191bd8211
홍석주 <hsj202201@gmail.com>
2025-05-02 11:23:22 +0900

    타이틀 변경

--- src/hooks/useDynamicTitle.js
+++ src/hooks/useDynamicTitle.js
@@ -8,9 +8,9 @@ function DynamicTitleHandler() {
   useEffect(() => {
     const path = location.pathname;
 
-    if (path.startsWith('/landing1')) {
+    if (path.startsWith('/missionpick')) {
       document.title = '크리에이터와 함께 만드는 콘텐츠, 미션 픽';
-    } else if (path.startsWith('/landing2')) {
+    } else if (path.startsWith('/mission.pick')) {
       document.title = '방송 밖에서도 스트리머와의 소통, 미션 픽';
     } else {
       document.title = ''; // 기본값
